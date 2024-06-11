6e99ced7d12afb76ba429a6af6150d1010131800
Dawid <dawid.marszalek99@gmail.com>
2024-06-11 22:23:44 +0200

    init: repo start

---
+++ src/main/java/com/dmhere/plantcaretracker/PlantCareTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dmhere.plantcaretracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantCareTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantCareTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmhere/plantcaretracker/PlantcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmhere.plantcaretracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantCareTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
